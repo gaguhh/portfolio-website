82d1a8e3a72fec2e50e6e00a329802a17a294b88
gaguhh <subru0994752@gmail.com>
2026-01-22 14:43:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function addRecommendation() {
+  const name = document.getElementById("rec-name").value;
+  const text = document.getElementById("rec-text").value;
 
+  if (name === "" || text === "") {
+    alert("Please fill all fields");
+    return;
+  }
+
+  const container = document.getElementById("recommendations");
+
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <p>${text}</p>
+    <h4>- ${name}</h4>
+  `;
+
+  container.appendChild(card);
+
+  document.getElementById("rec-name").value = "";
+  document.getElementById("rec-text").value = "";
+}
